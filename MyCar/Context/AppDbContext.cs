1ab0837e375eaa1bddb85c1c48e5ead549d1db84
Leonardo Menezes <leonhard.menezes@gmail.com>
2022-06-03 22:41:53 -0300

    Enviando projeto

---
+++ MyCar/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MyCar.Model;
+using System.IO;
+
+namespace MyCar.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+            
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+    }
+}
---
+++ MyCar/Model/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace MyCar.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Plate { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
